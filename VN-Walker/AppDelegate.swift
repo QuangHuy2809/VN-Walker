9a0e0e3ce318a8095e75efaaa09a1ff74037f1bb
Quang Huy <doanquanghuy@vnpt.vn>
2024-07-03 15:36:59 +0700

    Fix lint

--- VN-Walker/AppDelegate.swift
+++ VN-Walker/AppDelegate.swift
@@ -32,6 +32,7 @@ extension AppDelegate {
         navigationVC.setNavigationBarHidden(true, animated: false)
         navigationVC.navigationBar.barStyle = .black
         window?.rootViewController = navigationVC
+        // swiftlint:disable:next redundantType
         let loginVC: TestViewController = TestViewController.initFromStoryboard(feature: .login)
         navigationVC.pushViewController(loginVC, animated: false)
     }
